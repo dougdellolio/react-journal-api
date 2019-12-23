56395943d320d74e25244d8f1b8d76dce0ed6709
dougdellolio <doug.dellolio@gmail.com>
2019-12-23 11:39:08 -0500

    [Feature] Heroku db

--- day.py
+++ day.py
@@ -2,13 +2,36 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 class Day(db.Model):
-    id = db.Column(db.Date, primary_key=True)
+
+    __tablename__ = 'Days'
+
+    date = db.Column(db.Date, primary_key=True)
     breakfast = db.Column(db.String(80), unique=False, nullable=False)
     lunch = db.Column(db.String(120), unique=False, nullable=False)
     dinner = db.Column(db.String(120), unique=False, nullable=False)
     rating = db.Column(db.Integer, unique=False, nullable=False)
     notes = db.Column(db.String(max), unique=False, nullable=True)
+
+    def __init__(self, date, breakfast, lunch, dinner, rating, notes):
+        self.date = date
+        self.breakfast = breakfast
+        self.lunch = lunch
+        self.dinner = dinner
+        self.rating = rating
+        self.notes = notes
+
+    def __repr__(self):
+        return '<Date {}>'.format(self.Date)
+
+    def serialize(self):
+        return {
+            'date': self.date,
+            'breakfast': self.breakfast,
+            'lunch': self.lunch,
+            'dinner': self.dinner,
+            'rating': self.rating,
+            'notes': self.notes
+        }
